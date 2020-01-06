8eb72ba99564d2bd98330f3dff6bacb33d8dfdf6
ReddyTintaya <readyold@icloud.com>
2020-01-06 16:11:36 -0400

    realestate json

---
+++ app.py
@@ -0,0 +1,14 @@
+from fake_json import json_realestate
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello"
+
+@app.route("/")
+def hello():
+    return json_realestate
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
